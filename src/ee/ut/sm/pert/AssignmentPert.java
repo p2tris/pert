35c9ea921e85427e859074d4aac44c805c51fc43
Patris Halapuu <p2tris@gmail.com>
2013-10-14 20:53:02 +0300

    Writes .dot file
    Writes .dot file (without critical path yet as not excist)

--- src/ee/ut/sm/pert/AssignmentPert.java
+++ src/ee/ut/sm/pert/AssignmentPert.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -46,6 +48,7 @@ public class AssignmentPert {
         }
 
         System.out.println(nodeList);
+        convertToDoc(filename);
         return;
     }
 
@@ -57,5 +60,28 @@ public class AssignmentPert {
         }
 
         throw new Exception("Error in file, predecessor is not defined");  //To change body of created methods use File | Settings | File Templates.
+    }
+    
+    private static void convertToDoc (String filename) throws IOException{
+    	BufferedReader reader = new BufferedReader(new FileReader(filename));
+        String line;
+    	PrintWriter writer = new PrintWriter(filename+".dot", "UTF-8");
+    	writer.println("digraph graphname {");
+    	
+        while ((line=reader.readLine())!=null){
+            String[] linedata=line.split(",");
+            writer.println("     " + linedata[0] + " [label=\""+ linedata[0] + " (" + linedata[1] +" days)\"];");
+            for (int i=2;i<linedata.length;i++){
+                if (linedata[i].isEmpty()){
+                    continue;
+                }
+                else {
+                    writer.println("     " + linedata[i] + " -> " + linedata[0] + ";");
+                }
+            }
+        }
+        writer.println("}");
+        writer.close();
+        System.out.println("");
     }
 }

e0fd004cec7653c3d7be7de70c393949ca36ac7a
Patris Halapuu <p2tris@gmail.com>
2013-10-14 20:53:02 +0300

    Writes .dot file
    Writes .dot file (without critical path yet as not excist)
    (cherry picked from commit 35c9ea9)

--- src/ee/ut/sm/pert/AssignmentPert.java
+++ src/ee/ut/sm/pert/AssignmentPert.java
@@ -1,9 +1,13 @@
 package ee.ut.sm.pert;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -24,6 +28,8 @@ public class AssignmentPert {
         BufferedReader reader = new BufferedReader(new FileReader(filename));
         ArrayList<Node> nodeList = new ArrayList<Node>();
         String line;
+        while ((line=reader.readLine())!=null){
+            String[] linedata=line.split(",");
 
         while ((line = reader.readLine()) != null) {
             String[] linedata = line.split(",");
@@ -39,9 +45,15 @@ public class AssignmentPert {
                     Node predecessor = findPredecessor(nodeList, linedata[i]);
                     node.addPredecessor(predecessor);
                 }
+                else {
+                    Node predecessor=findPredecessor(nodeList,linedata[i]);
+                    node.predecessors.add(predecessor);
+                }
             }
         }
 
+        System.out.println(nodeList);
+        convertToDoc(filename);
         reader.close();
 
         List<Node> list = new ArrayList<Node>();
@@ -73,5 +85,28 @@ public class AssignmentPert {
         }
 
         throw new Exception("Error in file, predecessor is not defined");  //To change body of created methods use File | Settings | File Templates.
+    }
+    
+    private static void convertToDoc (String filename) throws IOException{
+    	BufferedReader reader = new BufferedReader(new FileReader(filename));
+        String line;
+    	PrintWriter writer = new PrintWriter(filename+".dot", "UTF-8");
+    	writer.println("digraph graphname {");
+    	
+        while ((line=reader.readLine())!=null){
+            String[] linedata=line.split(",");
+            writer.println("     " + linedata[0] + " [label=\""+ linedata[0] + " (" + linedata[1] +" days)\"];");
+            for (int i=2;i<linedata.length;i++){
+                if (linedata[i].isEmpty()){
+                    continue;
+                }
+                else {
+                    writer.println("     " + linedata[i] + " -> " + linedata[0] + ";");
+                }
+            }
+        }
+        writer.println("}");
+        writer.close();
+        System.out.println("");
     }
 }
